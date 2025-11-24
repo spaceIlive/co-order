65754c5c9ad465696b5c349674ffbd5d7f00d903
YoonGwan <gyun6266@gmail.com>
2025-11-24 17:50:58 +0900

    feat: add member address update flow

---
+++ src/main/java/gwan/co_order/controller/MemberAddressForm.java
@@ -0,0 +1,29 @@
+package gwan.co_order.controller;
+
+import gwan.co_order.domain.Address;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberAddressForm {
+
+    @NotEmpty(message = "주소는 필수입니다")
+    private String address;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    public static MemberAddressForm from(Address address) {
+        MemberAddressForm form = new MemberAddressForm();
+        if (address != null) {
+            form.setAddress(address.getAddress());
+            form.setLatitude(address.getLatitude());
+            form.setLongitude(address.getLongitude());
+        }
+        return form;
+    }
+}
+
